b301e603b7ca2cf0cf558e8c312cc53f9674f092
karikomi <karikomi.ipod@gmail.com>
2022-01-31 22:54:04 +0900

    init

---
+++ .routify/routes.default.js
@@ -0,0 +1,45 @@
+
+
+export default {
+  "meta": {},
+  "id": "_default",
+  "file": {
+    "path": "src/routes",
+    "dir": "src",
+    "base": "routes",
+    "ext": "",
+    "name": "routes"
+  },
+  "rootName": "default",
+  "routifyDir": import.meta.url,
+  "children": [
+    {
+      "meta": {},
+      "id": "_default_index_md",
+      "name": "index",
+      "module": () => import('../src/routes/index.md'),
+      "file": {
+        "path": "src/routes/index.md",
+        "dir": "src/routes",
+        "base": "index.md",
+        "ext": ".md",
+        "name": "index"
+      },
+      "children": []
+    },
+    {
+      "meta": {},
+      "id": "_default_index_svelte",
+      "name": "index",
+      "module": () => import('../src/routes/index.svelte'),
+      "file": {
+        "path": "src/routes/index.svelte",
+        "dir": "src/routes",
+        "base": "index.svelte",
+        "ext": ".svelte",
+        "name": "index"
+      },
+      "children": []
+    }
+  ]
+}
